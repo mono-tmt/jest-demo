60f3054761050c5eac86412b88ea7da723867ca9
mono-tmt <tomita@monosus.co.jp>
2021-07-29 14:02:05 +0900

    :sparkles: Add test for useBreadList

---
+++ src/__tests__/useBreadList.test.js
@@ -0,0 +1,23 @@
+import { expect, test } from "@jest/globals";
+import { render } from "@testing-library/react";
+import useBreedList from "../useBreedList.js";
+
+function getBreedList(animal) {
+  let list;
+
+  function TestComponent() {
+    list = useBreedList(animal);
+    return null;
+  }
+
+  render(<TestComponent />);
+
+  return list;
+}
+
+test("gives an empty list with no animal", async () => {
+  const [breedList, status] = getBreedList();
+
+  expect(breedList).toHaveLength(0);
+  expect(status).toBe("unloaded");
+});
